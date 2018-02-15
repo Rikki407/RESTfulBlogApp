256daaa316fd9c4da52c6c708834037568398271
Rikki407 <rishablamba407@gmail.com>
2018-02-16 00:31:07 +0530

    added delete functionality

--- app.js
+++ app.js
@@ -83,6 +83,18 @@ app.put("/blogs/:id",function (req, res) {
        }
    })
 });
+
+//DELETE ROUTE
+app.delete("/blogs/:id",function (req, res) {
+    Blogs.findByIdAndRemove(req.params.id,function (err) {
+       if(err){
+           res.redirect("/blogs");
+       } else{
+           res.redirect("/blogs");
+       }
+    });
+});
+
 app.listen(3000, function () {
    console.log("Blogs App Started on port 3000");
 });
