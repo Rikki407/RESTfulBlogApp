48fc19087b0b9c9a6bed3e66dba6f12c3d759d8c
Rikki407 <rishablamba407@gmail.com>
2018-02-15 18:06:51 +0530

    added create functionality

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ var Blog = mongoose.model("blogs",blogSchema);
 app.get("/",function (req, res) {
    res.redirect("/blogs");
 });
+//INDEX ROUTE
 app.get("/blogs",function (req, res) {
     Blog.find({},function (err, allBlogs) {
        if(err){
@@ -33,6 +34,20 @@ app.get("/blogs",function (req, res) {
            res.render("index",{blogs : allBlogs});
        }
     });
+});
+//CREATE
+app.post("/blogs",function (req, res) {
+   Blog.create(req.body.blog,function (err, newBlog) {
+      if(err){
+          res.render("new");
+      }else {
+          res.redirect("/blogs");
+      }
+   });
+});
+//NEW ROUTE
+app.get("/blogs/new",function (req, res) {
+   res.render("new");
 });
 app.listen(3000, function () {
    console.log("Blog App Started on port 3000");
