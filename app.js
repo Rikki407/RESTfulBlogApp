193192079911cae85da697aeabf76230d1892378
Rikki407 <rishablamba407@gmail.com>
2018-02-15 23:56:50 +0530

    added update blog functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express     = require("express"),
     mongoose    = require("mongoose"),
+    methodOverride = require("method-override"),
     bodyParser  = require("body-parser");
 var app  = express();
 //APP CONFIG
@@ -7,6 +8,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 //MONGOOSE MODEL CONFIG
 var blogSchema = new mongoose.Schema(
@@ -60,7 +62,27 @@ app.get("/blogs/:id",function (req, res) {
         }
     });
 });
+//EDIT ROUTE
+app.get("/blogs/:id/edit",function (req, res) {
+    Blogs.findById(req.params.id,function (err , blog) {
+       if(err){
+           res.redirect("/blogs");
+       } else{
+           res.render("edit",{blog:blog});
+       }
+    });
+});
 
+//UPDATE ROUTE
+app.put("/blogs/:id",function (req, res) {
+   Blogs.findByIdAndUpdate(req.params.id, req.body.blog, function (err , newBlog) {
+       if(err){
+           console.log(err);
+       }else {
+           res.redirect("/blogs/"+req.params.id);
+       }
+   })
+});
 app.listen(3000, function () {
    console.log("Blogs App Started on port 3000");
 });
